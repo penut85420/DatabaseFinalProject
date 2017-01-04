552258381c886c5c5a168e37e69f182fb946f0b6
Penut Chen <penut85420@gmail.com>
2017-01-04 15:07:27 +0800

    Delete About

--- com/menu.php
+++ com/menu.php
@@ -4,6 +4,5 @@
         <th><a href = "IdolCard.php">【卡片檢視】</a></th>
         <th><a href = "IdolSetting.php">【偶像設定】</a></th>
         <th><a href = "Producer.php">【製作人】</a></th>
-        <th><a href = "About.php">【關於】</a></th>
     </tr>
 </table>
